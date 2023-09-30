990d0a7df889190b8fc2128adec3a55faec754cf
fbzavaleta <benjamin1992.1992@gmail.com>
2023-10-01 00:12:29 +0100

    adding tivaware

---
+++ tivaware/sensorlib/gcc/isl29023.d
@@ -0,0 +1,11 @@
+gcc/isl29023.o: isl29023.c \
+ /home/benji/TexasIn/gcc-arm-none-eabi-5_4-2016q3/lib/gcc/arm-none-eabi/5.4.1/include/stdint.h \
+ /home/benji/TexasIn/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/stdint.h \
+ /home/benji/TexasIn/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/machine/_default_types.h \
+ /home/benji/TexasIn/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/sys/features.h \
+ /home/benji/TexasIn/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/_newlib_version.h \
+ /home/benji/TexasIn/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/sys/_intsup.h \
+ /home/benji/TexasIn/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/sys/_stdint.h \
+ /home/benji/TexasIn/gcc-arm-none-eabi-5_4-2016q3/lib/gcc/arm-none-eabi/5.4.1/include/stdbool.h \
+ ../sensorlib/hw_isl29023.h ../sensorlib/i2cm_drv.h \
+ ../sensorlib/isl29023.h
