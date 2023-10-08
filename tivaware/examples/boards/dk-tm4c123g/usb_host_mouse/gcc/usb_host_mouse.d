a88aeaa94e87fa5dfd64b77ebe4faaa12d83f6d9
fbzavaleta <benjamin1992.1992@gmail.com>
2023-10-08 12:07:35 +0100

    updating tivaware package

---
+++ tivaware/examples/boards/dk-tm4c123g/usb_host_mouse/gcc/usb_host_mouse.d
@@ -0,0 +1,35 @@
+gcc/usb_host_mouse.o: usb_host_mouse.c \
+ /home/benji/TexasIn/gcc-arm-none-eabi-5_4-2016q3/lib/gcc/arm-none-eabi/5.4.1/include/stdbool.h \
+ /home/benji/TexasIn/gcc-arm-none-eabi-5_4-2016q3/lib/gcc/arm-none-eabi/5.4.1/include/stdint.h \
+ /home/benji/TexasIn/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/stdint.h \
+ /home/benji/TexasIn/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/machine/_default_types.h \
+ /home/benji/TexasIn/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/sys/features.h \
+ /home/benji/TexasIn/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/_newlib_version.h \
+ /home/benji/TexasIn/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/sys/_intsup.h \
+ /home/benji/TexasIn/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/sys/_stdint.h \
+ ../../../../inc/hw_memmap.h ../../../../inc/hw_types.h \
+ ../../../../driverlib/gpio.h ../../../../driverlib/pin_map.h \
+ ../../../../driverlib/rom.h ../../../../driverlib/sysctl.h \
+ ../../../../usblib/usblib.h ../../../../usblib/usbhid.h \
+ ../../../../grlib/grlib.h ../../../../usblib/host/usbhost.h \
+ ../../../../usblib/host/usbhhid.h ../../../../usblib/host/usbhhidmouse.h \
+ ../../../../utils/ustdlib.h \
+ /home/benji/TexasIn/gcc-arm-none-eabi-5_4-2016q3/lib/gcc/arm-none-eabi/5.4.1/include/stdarg.h \
+ /home/benji/TexasIn/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/time.h \
+ /home/benji/TexasIn/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/_ansi.h \
+ /home/benji/TexasIn/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/newlib.h \
+ /home/benji/TexasIn/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/sys/config.h \
+ /home/benji/TexasIn/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/machine/ieeefp.h \
+ /home/benji/TexasIn/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/sys/cdefs.h \
+ /home/benji/TexasIn/gcc-arm-none-eabi-5_4-2016q3/lib/gcc/arm-none-eabi/5.4.1/include/stddef.h \
+ /home/benji/TexasIn/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/sys/reent.h \
+ /home/benji/TexasIn/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/_ansi.h \
+ /home/benji/TexasIn/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/sys/_types.h \
+ /home/benji/TexasIn/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/machine/_types.h \
+ /home/benji/TexasIn/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/sys/lock.h \
+ /home/benji/TexasIn/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/machine/time.h \
+ /home/benji/TexasIn/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/sys/types.h \
+ /home/benji/TexasIn/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/machine/types.h \
+ /home/benji/TexasIn/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/sys/timespec.h \
+ /home/benji/TexasIn/gcc-arm-none-eabi-5_4-2016q3/arm-none-eabi/include/sys/_timespec.h \
+ ../drivers/cfal96x64x16.h
